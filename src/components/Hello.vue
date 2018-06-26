2dc19b63bdfd239a3ac358161eb77d991c57c75c
fanlongbiao <fanlongbiao@tanlunet.com>
2018-06-26 17:33:50 +0800

    message 12

---
+++ src/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+ new Vue({
+     el:'#hello',
+     data: {
+         message:'hello vue.js'
+     }
+
+ })
+</script>
